361a39e95cc0685270f527a3885a4b7b788f8edf
SOARESGG10 <spectggof@gmail.com>
2023-06-03 22:26:23 -0300

    📑 | Add: seller entity class

--- src/br/soares/application/Main.java
+++ src/br/soares/application/Main.java
@@ -1,12 +1,19 @@
 package br.soares.application;
 
 import br.soares.model.entities.Department;
+import br.soares.model.entities.Seller;
+
+import java.util.Date;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(1, "David", "david@gmail.com", new Date(), 5500.00, department);
+
         System.out.println(department);
+        System.out.println(seller);
     }
 
 
